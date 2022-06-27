1318441ec4f7a4c64c7e632e4620c9d8683f3bf3
Martin Lablans <6804500+lablans@users.noreply.github.com>
2022-06-27 14:41:41 +0200

    Remove dependency on warp. (#1)
    Remove dependency on warp. Refactor.

--- central/src/main.rs
+++ central/src/main.rs
@@ -17,15 +17,9 @@
 //!  - Authorization: Basic(MyUUID, secretString)
 //!
 
-mod errors;
-
-#[cfg(feature = "backend-warp")]
-mod logic_warp;
-#[cfg(feature = "backend-warp")]
-mod serve_warp;
-
-#[cfg(feature = "backend-axum")]
 mod serve_axum;
+#[cfg(debug_assertions)]
+mod devhelper;
 
 use std::{collections::HashMap, sync::Arc};
 
@@ -35,7 +29,7 @@ use tokio::sync::RwLock;
 #[tokio::main]
 pub async fn main() {
     #[cfg(debug_assertions)]
-    if print_example_objects() == true { return; }
+    if devhelper::print_example_objects() == true { return; }
 
     let tasks: HashMap<MsgId, MsgTaskRequest> = HashMap::new();
     let (new_tasks_tx, _) = tokio::sync::broadcast::channel::<MsgTaskRequest>(512);
@@ -43,29 +37,5 @@ pub async fn main() {
     let tasks = Arc::new(RwLock::new(tasks));
     let new_tasks_tx = Arc::new(new_tasks_tx);
 
-    #[cfg(feature = "backend-warp")]
-    serve_warp::serve_warp(tasks, new_tasks_tx).await;
-
-    #[cfg(feature = "backend-axum")]
     serve_axum::serve_axum(tasks, new_tasks_tx).await;
-}
-
-#[cfg(debug_assertions)]
-fn print_example_objects() -> bool {
-    if std::env::args().nth(1).unwrap_or_default() == "examples" {
-        let tasks = generate_example_tasks();
-        let mut num_tasks = 0;
-        let mut num_results = 0;
-        for task in tasks.values() {
-            println!("export TASK{}='{}'", num_tasks, serde_json::to_string(task).unwrap().replace("'", "\'"));
-            num_tasks = num_tasks + 1;
-            for result in task.results.values() {
-                println!("export RESULT{}='{}'", num_results, serde_json::to_string(result).unwrap().replace("'", "\'"));
-                num_results = num_results + 1;
-            }
-        }
-        return true;
-    } else {
-        return false;
-    }
 }
