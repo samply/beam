3a72ee0aeb257b4d0a6b0fec2c04c590b955b7b5
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2022-06-28 15:13:59 +0200

    Removed outdated documentation

--- central/src/main.rs
+++ central/src/main.rs
@@ -1,22 +1,3 @@
-//! # Broker (Central Component)
-//! ## API
-//! POST /tasks/ ==> Create Task. Give MsgTask without ID, return task_uuid
-//! GET /tasks/ ==> Get all tasks where I am "to"
-//! PUT /tasks/{task_uuid}/results/{worker_uuid} ==> Update existing result
-//! GET /tasks/{task_uuid}/results/ ==> Give all results for task uuid
-//! GET /tasks/{task_uuid}/results/{worker_uuid} ==> Give that specific result
-//!
-//! ### Parameters:
-//! ####  Long Polling
-//! - (default) Don't block; return results immediately
-//! - &block.timeout=1500ms => Return whatever is there after 1500ms
-//! - &block.results=5 => Block forever until 5 results have arrived
-//! - &block.timeout=1500ms&block.results=5 => Block for 1500ms OR if >= 5 results have arrived (whichever comes first)
-//!
-//! ### Headers:
-//!  - Authorization: Basic(MyUUID, secretString)
-//!
-
 mod serve_axum;
 #[cfg(debug_assertions)]
 mod devhelper;
