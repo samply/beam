7c12280459282e2e6350c70948265dc8022823a9
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2022-07-26 09:42:55 +0200

    Respect logging variable RUST_LOG, detailed log for debug, short log for release

--- shared/src/logger.rs
+++ shared/src/logger.rs
@@ -1,19 +1,27 @@
-use tracing::{Level, dispatcher::SetGlobalDefaultError, debug, info};
+use tracing::{dispatcher::SetGlobalDefaultError, Level, debug};
 
 pub fn init_logger() -> Result<(), SetGlobalDefaultError>{
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
         .with_max_level(Level::DEBUG);
 
-    // let subscriber = subscriber
-    //     .with_env_filter("hyper=warn,rustify=warn,vaultrs=warn,info");
+    let env_filter = match std::env::var("RUST_LOG") {
+        Ok(env) if ! env.is_empty() => {
+            env
+        },
+        _ => {
+            if cfg!(debug_assertions) {
+                "hyper=warn,rustify=warn,vaultrs=warn,info".to_string()
+            } else {
+                "hyper=warn,rustify=warn,vaultrs=warn,debug".to_string()
+            }
+        }
+    };
 
-    // #[cfg(debug_assertions)]
-    // let subscriber = subscriber
-    //     .with_env_filter("hyper=warn,rustify=warn,vaultrs=warn,debug");
-
-    let subscriber = subscriber.finish();
+    let subscriber = subscriber
+        .with_env_filter(env_filter.clone())
+        .finish();
     tracing::subscriber::set_global_default(subscriber)?;
     
-    debug!("Logging initialized.");
+    debug!("Logging initialized with env_filter {env_filter}.");
     Ok(())
 }
