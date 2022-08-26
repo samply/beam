5b6ab2b7b4e3e470a985021b89f82a80155ea0fa
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2022-08-26 13:32:57 +0200

    squash s well

--- dev/example_json.sh
+++ dev/example_json.sh
@@ -87,7 +87,6 @@ export RESULT_BY_APP1_P2='
   ],
   "task": "6f531223-3699-4f6e-b7bf-88d8064fea7e",
   "status": "claimed",
-  "body": "test",
   "metadata": {
     "complex": "A map (key 'complex') is possible, too"
   }
