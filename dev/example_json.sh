e533cfc82d748f389b815ff3f61197d568494653
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2022-10-11 07:20:54 +0000

    Revert "Dirty commit: add new test WIP"
    This reverts commit 7915ee830a639fec1e2e3a1561cb8de237b1a29c.

--- dev/example_json.sh
+++ dev/example_json.sh
@@ -79,25 +79,6 @@ export TASK2_BY_A1P1_FOR_A1P2='
   "ttl": [TTL],
   "metadata": null
 }'
-export TASK_BY_A1P1_FOR_A1PALL='
-{
-  "id": "dfa0dbe1-46f7-32da-b3f5-7b13dc92caae",
-  "from": "app1.proxy1.broker",
-  "to": [
-    "app1.proxy1.broker",
-    "app1.proxy2.broker",
-    "app1.proxy3.broker"
-  ],
-  "body": "So much work!",
-  "failure_strategy": {
-    "retry": {
-      "backoff_millisecs": 1000,
-      "max_tries": 5
-    }
-  },
-  "ttl": [TTL],
-  "metadata": null
-}'
 export RESULT_BY_APP1_P2='
 {
   "from": "app1.proxy2.broker",
@@ -106,57 +87,6 @@ export RESULT_BY_APP1_P2='
   ],
   "task": "6f531223-3699-4f6e-b7bf-88d8064fea7e",
   "status": "claimed",
-  "metadata": {
-    "complex": "A map (key 'complex') is possible, too"
-  }
-}'
-export MULTI_CLAIM_BY_A1P1='
-{
-  "from": "app1.proxy1.broker",
-  "to": [
-    "app1.proxy1.broker"
-  ],
-  "task": "dfa0dbe1-46f7-32da-b3f5-7b13dc92caae",
-  "status": "claimed",
-  "metadata": {
-    "complex": "A map (key 'complex') is possible, too"
-  }
-}'
-export MULTI_RESULT_BY_A1P1='
-{
-  "from": "app1.proxy1.broker",
-  "to": [
-    "app1.proxy1.broker"
-  ],
-  "task": "dfa0dbe1-46f7-32da-b3f5-7b13dc92caae",
-  "status": "succeeded",
-  "body": "A1P1 successfully did the thing",
-  "metadata": {
-    "complex": "A map (key 'complex') is possible, too"
-  }
-}'
-export MULTI_RESULT_BY_A1P2='
-{
-  "from": "app1.proxy2.broker",
-  "to": [
-    "app1.proxy1.broker"
-  ],
-  "task": "dfa0dbe1-46f7-32da-b3f5-7b13dc92caae",
-  "status": "succeeded",
-  "body": "A1P2 successfully did the thing",
-  "metadata": {
-    "complex": "A map (key 'complex') is possible, too"
-  }
-}'
-export MULTI_RESULT_BY_A1P3='
-{
-  "from": "app1.proxy3.broker",
-  "to": [
-    "app1.proxy3.broker"
-  ],
-  "task": "dfa0dbe1-46f7-32da-b3f5-7b13dc92caae",
-  "status": "succeeded",
-  "body": "A1P3 successfully did the thing",
   "metadata": {
     "complex": "A map (key 'complex') is possible, too"
   }
