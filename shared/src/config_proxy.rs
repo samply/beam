bca7b48f0792bfe3bf5e50502035805ec16bcc23
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2022-11-07 11:20:07 +0100

    Add Retry mechanism into proxy for startup

--- shared/src/config_proxy.rs
+++ shared/src/config_proxy.rs
@@ -7,6 +7,7 @@ use axum::http::HeaderValue;
 use hyper::Uri;
 use serde::Deserialize;
 use tracing::{info, debug};
+use tokio_retry::strategy::ExponentialBackoff;
 
 use crate::{errors::SamplyBeamError, beam_id::{BeamId, ProxyId, AppId, self, BrokerId}};
 
@@ -17,7 +18,8 @@ pub struct Config {
     pub bind_addr: SocketAddr,
     pub proxy_id: ProxyId,
     pub api_keys: HashMap<AppId,ApiKey>,
-    pub tls_ca_certificates: Vec<X509>
+    pub tls_ca_certificates: Vec<X509>,
+    pub com_retry: ExponentialBackoff,
 }
 
 pub type ApiKey = String;
@@ -90,13 +92,16 @@ impl crate::config::Config for Config {
         }
         let tls_ca_certificates = crate::crypto::load_certificates_from_dir(cli_args.tls_ca_certificates_dir)
             .map_err(|e| SamplyBeamError::ConfigurationFailed(format!("Unable to read from TLS CA directory: {}", e)))?;
+        let com_retry = tokio_retry::strategy::ExponentialBackoff::from_millis(50)
+            .max_delay(std::time::Duration::new(10,0)); // 10 seconds max delay
         let config = Config {
             broker_host_header: uri_to_host_header(&cli_args.broker_url)?,
             broker_uri: cli_args.broker_url,
             bind_addr: cli_args.bind_addr,
             proxy_id,
             api_keys,
-            tls_ca_certificates
+            tls_ca_certificates,
+            com_retry
         };
         info!("Successfully read config and API keys from CLI and secrets file.");
         Ok(config)
