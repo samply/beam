c0b7526761044dcb69687286a57bcfa6dd28b7de
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2022-12-02 20:59:34 +0000

    Remove dependency on tower, ctrlc crates

--- broker/src/serve.rs
+++ broker/src/serve.rs
@@ -8,7 +8,7 @@ use axum::{
 use serde::{Deserialize};
 use shared::{MsgTaskRequest, MsgTaskResult, MsgId, HowLongToBlock, HasWaitId, MsgSigned, MsgEmpty, Msg, EMPTY_VEC_APPORPROXYID, config};
 use tokio::{sync::{broadcast::{Sender, Receiver}, RwLock}, time};
-use tracing::{debug, info, trace};
+use tracing::{debug, info, trace, warn};
 
 use crate::{serve_tasks, serve_health, serve_pki, crypto::GetCertsFromPki};
 
@@ -20,16 +20,21 @@ pub(crate) async fn serve() -> anyhow::Result<()> {
         .merge(serve_health::router());
 
     // Graceful shutdown handling
-    let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+    let (shutdown_tx, mut shutdown_rx) = tokio::sync::mpsc::channel(1);
 
-    ctrlc::set_handler(move || tx.blocking_send(()).expect("Could not send shutdown signal on channel."))
-        .expect("Error setting handler for graceful shutdown.");
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await
+                .expect("Unable to listen for Ctrl+C for graceful shutdown");
+        if shutdown_tx.blocking_send(()).is_err() {
+            warn!("Unable to send signal for clean shutdown... ignoring.");
+        }
+    });
 
     info!("Listening for requests on {}", config::CONFIG_CENTRAL.bind_addr);
     axum::Server::bind(&config::CONFIG_CENTRAL.bind_addr)
         .serve(app.into_make_service())
         .with_graceful_shutdown(async {
-            rx.recv().await;
+            shutdown_rx.recv().await;
             info!("Shutting down.");
         })
         .await?;
