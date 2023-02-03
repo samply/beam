d6261e540ec60cb16411eb6434e2669c96e1a5c7
Tobias Kussel <TKussel@users.noreply.github.com>
2023-02-03 14:38:52 +0100

    Update dev/test_5_large_requests.sh
    Co-authored-by: Martin Lablans <6804500+lablans@users.noreply.github.com>

--- dev/test_5_large_requests.sh
+++ dev/test_5_large_requests.sh
@@ -11,13 +11,3 @@ fi
 
 success
 
-#testing Deliver large result \#1
-#
-#RET=$(echo "${LARGE_RESULT_BY_A1P1}" | curl_put $APP1_P1 -v $P1/v1/tasks/70c0ca90-bfcf-4312-a6af-42cbd57dc0b8/results/app1.proxy1.broker)
-#CODE=$(echo $RET | jq -r .response_code)
-#
-#if [ "$CODE" != "201" ]; then
-    #fail "$RET" Expected code 201, got $CODE
-#fi
-#
-#success
