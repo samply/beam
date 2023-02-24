fa89dfffed013e7d25fe2c189d1f3931b0d28606
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2023-02-24 16:45:40 +0100

    Read X-FORWARDED-FOR header

--- shared/src/middleware.rs
+++ shared/src/middleware.rs
@@ -1,13 +1,18 @@
+use std::net::IpAddr;
+
 use axum::{middleware::{self, Next}, response::{Response, IntoResponse}, body::HttpBody};
-use http::{Request, StatusCode, header};
+use http::{Request, StatusCode, header::{self, HeaderName}, HeaderValue};
 use hyper::Body;
 use tracing::{info, warn, span, Level, instrument};
 
-#[instrument(skip_all,fields(req = make_line(&req)))]
+const X_FORWARDED_FOR: HeaderName = HeaderName::from_static("x-forwarded-for");
+
 pub async fn log(req: Request<Body>, next: Next<Body>) -> Response {
-    // let log = format!("<= {} {}", req.method(), req.uri());
-    // span!(Level::INFO, log);
-    let mut line = format!("<= {} {}", req.method(), req.uri());
+    let ip = get_ip(&req)
+        .and_then(|f| Some(f.to_string()))
+        .unwrap_or("NO_IP_HEADER".into());
+    
+    let mut line = format!("({}) <= {} {}", ip, req.method(), req.uri());
     let resp = next.run(req).await;
     #[allow(clippy::format_push_string)] // see https://github.com/rust-lang/rust-clippy/issues/9077
     line.push_str(&format!(" || => {}", resp.status()));
@@ -19,6 +24,16 @@ pub async fn log(req: Request<Body>, next: Next<Body>) -> Response {
     resp
 }
 
-fn make_line(req: &Request<Body>) -> String {
-    format!("{} {}", req.method(), req.uri())
+fn get_ip(req: &Request<Body>) -> Option<IpAddr> {
+    if let Some(ip_from_header) = req.headers().get(X_FORWARDED_FOR) {
+        let a = ip_from_header
+            .to_str()
+            .ok()?
+            .split(',').next().unwrap_or_default()
+            .parse()
+            .ok()?;
+        Some(a)
+    } else {
+        None
+    }
 }
