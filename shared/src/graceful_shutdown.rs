8cb591869acbba9f180a72f20da0d8aa1ac7fc8a
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2023-03-15 17:09:47 +0100

    Make it work on Windows, too

--- shared/src/graceful_shutdown.rs
+++ shared/src/graceful_shutdown.rs
@@ -3,9 +3,12 @@ use std::time::Duration;
 use tokio::signal::unix::{signal,SignalKind};
 use tracing::info;
 
+#[cfg(unix)]
 pub async fn wait_for_signal() {
-    let mut sigterm = signal(SignalKind::terminate()).unwrap();
-    let mut sigkill = signal(SignalKind::interrupt()).unwrap();
+    let mut sigterm = signal(SignalKind::terminate())
+        .expect("Unable to register shutdown handler; are you running a Unix-based OS?");
+    let mut sigkill = signal(SignalKind::interrupt())
+        .expect("Unable to register shutdown handler; are you running a Unix-based OS?");
     let signal = tokio::select! {
         _ = sigterm.recv() => "SIGTERM",
         _ = sigkill.recv() => "SIGKILL"
@@ -13,4 +16,12 @@ pub async fn wait_for_signal() {
     // The following does not print in docker-compose setups but it does when run individually.
     // Probably a docker-compose error.
     info!("Received signal ({signal}) - shutting down gracefully.");
+}
+
+#[cfg(windows)]
+pub async fn wait_for_signal() {
+    if let Err(e) = tokio::signal::ctrl_c().await {
+        panic!("Unable to register shutdown handler: {e}.");
+    }
+    info!("Received shutdown signal - shutting down gracefully.");
 }
