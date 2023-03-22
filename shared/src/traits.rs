3275961aac506c8271e4b47a39e35342ea3c20c0
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-03-22 11:04:37 +0000

    Add better duration parsing

--- shared/src/traits.rs
+++ shared/src/traits.rs
@@ -1,12 +1,21 @@
 use axum::{async_trait, extract::{FromRequest, Query, Path, FromRequestParts, self}, BoxError, http::StatusCode, RequestPartsExt};
+use fundu::DurationParser;
 use http::request::Parts;
 
 use crate::*;
 
 #[derive(Deserialize)]
-struct HowLongToBlockAsIntegers {
-    wait_time: Option<u64>,
+struct HowLongToBlockQueryExtractor {
+    wait_time: Option<String>,
     wait_count: Option<u16>,
+}
+
+#[test]
+fn test_duration_parsing() { 
+    let mut parser = DurationParser::default();
+    let parser = parser.default_unit(fundu::TimeUnit::MilliSecond);
+    assert_eq!(parser.parse("1234s").unwrap().as_millis(), 1234000);
+    assert_eq!(parser.parse("1234").unwrap().as_millis(), 1234);
 }
 
 #[async_trait]
@@ -17,10 +26,17 @@ S: Send + Sync
     type Rejection = (StatusCode, &'static str);
 
     async fn from_request_parts(req: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
-        match req.extract::<Query<HowLongToBlockAsIntegers>>().await {
-            Ok(value) => {
-                let wait_time = value.0.wait_time.map(Duration::from_millis);
-                Ok(Self { wait_time, wait_count: value.0.wait_count })
+        match req.extract::<Query<HowLongToBlockQueryExtractor>>().await {
+            Ok(Query(HowLongToBlockQueryExtractor { wait_time, wait_count })) => {
+                if let Some(wait_time_str) = wait_time {
+                    let wait_time = DurationParser::default()
+                        .default_unit(fundu::TimeUnit::MilliSecond)
+                        .parse(&wait_time_str)
+                        .map_err(|_| (StatusCode::BAD_REQUEST, "For long-polling, please define &wait_time=<millisecs> and &wait_count=<count>."))?;
+                    Ok(Self { wait_time: Some(wait_time), wait_count})
+                } else {
+                    Ok(Self { wait_time: None, wait_count })
+                }
             },
             Err(_) => Err((StatusCode::BAD_REQUEST, "For long-polling, please define &wait_time=<millisecs> and &wait_count=<count>.")),
         }
