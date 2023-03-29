7b32c4acc6a4e2fec2401c5f6dcee719c7463a61
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-03-29 15:39:08 +0000

    Show events in frontend

--- proxy/monitor/src/lib/RequestList.svelte
+++ proxy/monitor/src/lib/RequestList.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
-    let requests = [{"test": "test"}];
+    import { onMount } from "svelte";
+
+
+    let requests = [];
+
+    onMount(() => {
+        // TODO change this to actual host
+        let sse_stream = new EventSource("http://localhost:8082/monitor/events");
+        sse_stream.addEventListener("message", (e) => {
+            // We cant push as we need svelte to understand that we updated this and need to rerender
+            requests = [...requests, e.data];
+        })
+    })
 </script>
 
 <header>
--- proxy/monitor/vite.config.ts
+++ proxy/monitor/vite.config.ts
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: "../dist"
+    outDir: "../dist",
+    emptyOutDir: true,
   }
 })
