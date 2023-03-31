60cb038ade1570c2b78ec6c8ccee352fd76c5ad9
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-03-31 14:39:20 +0000

    Expandable component

---
+++ proxy/monitor/src/lib/Expandable.svelte
@@ -0,0 +1,15 @@
+
+<script lang="ts">
+    let expanded = false;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div on:click|stopPropagation={() => expanded = !expanded} class="head">
+    <slot name="head">
+        Expand
+    </slot>
+</div>
+
+<div  hidden={!expanded}>
+    <slot />
+</div>
