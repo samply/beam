a8b8dede8b10e0b4234dc86b1efe2ad8d00b0445
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-05-02 15:34:08 +0000

    Add first tests

--- tests/socket_test.rs
+++ tests/socket_test.rs
@@ -1,6 +1,14 @@
 
+use std::time::{SystemTime, Duration};
+
 use fast_socks5::client::{Config, Socks5Stream};
+use http::{Request, HeaderName, header, method, Method, Response, StatusCode};
+use hyper::Body;
+use shared::{http_client::SamplyHttpClient, MsgSocketRequest, beam_id::{AppOrProxyId, AppId, BeamId}, Plain, MyUuid};
 use tokio::io::{AsyncWriteExt, AsyncReadExt};
+use serde_json::Value;
+use anyhow::Result;
+use tests::*;
 
 #[tokio::test]
 async fn test_sockets() {
@@ -16,4 +24,33 @@ async fn test_sockets() {
     let recieved = &mut [0 as u8; TEST_DATA.len()];
     b.read_exact(recieved).await.unwrap();
     assert_eq!(TEST_DATA, recieved);
+}
+
+#[tokio::test]
+async fn test_full() -> Result<()> {
+    let client = shared::http_client::build(&Vec::new(), None, None)?;
+    let mut res = post_socket_req(client).await?;
+    let body = hyper::body::to_bytes(res.body_mut()).await?;
+    println!("response: {}", String::from_utf8(body.to_vec())?);
+    assert_eq!(res.status(), StatusCode::CREATED);
+    Ok(())
+}
+
+async fn post_socket_req(client: SamplyHttpClient) -> Result<Response<Body>> {
+    let task = MsgSocketRequest {
+        from: APP1.clone(),
+        to: APP2.clone(),
+        expire: SystemTime::now() + Duration::from_secs(60),
+        id: MyUuid::new(),
+        secret: Plain::from("test"),
+        metadata: Value::Null,
+        result: None,
+    };
+    let req = Request::builder()
+        .uri(format!("{PROXY1}/v1/sockets"))
+        .header(header::AUTHORIZATION, format!("ApiKey {} {APP_KEY}", APP1.clone()))
+        .method(Method::POST)
+        .body(hyper::Body::from(serde_json::to_vec(&task)?))?;
+    let resp = client.request(req).await?;
+    Ok(resp)
 }
---
+++ tests/src/lib.rs
@@ -0,0 +1,25 @@
+
+use once_cell::sync::Lazy;
+use shared::beam_id::{AppOrProxyId, BeamId, AppId};
+
+pub const APP1: Lazy<AppOrProxyId> = Lazy::new(|| {
+    AppId::set_broker_id("broker".to_string());
+    AppOrProxyId::new(option_env!("APP1_P1").unwrap_or("app1.proxy1.broker")).unwrap()
+}); 
+pub const APP2: Lazy<AppOrProxyId> = Lazy::new(|| AppOrProxyId::new(option_env!("APP2_P2").unwrap_or("app2.proxy2.broker")).unwrap()); 
+
+// unwrap_or is not const yet
+pub const PROXY1: &str = match option_env!("P1") {
+    Some(v) => v,
+    _ => "http://localhost:8081"
+};
+pub const PROXY2: &str = match option_env!("P2") {
+    Some(v) => v,
+    _ => "http://localhost:8082"
+};
+
+pub const APP_KEY: &str = match option_env!("APP_KEY") {
+    Some(v) => v,
+    None => "App1Secret"
+};
+
