cb087e85c9a82ed233a578c24ec8ba5d0ba64c34
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-06-02 08:56:30 +0000

    Proxy health reporting endpoints for the broker

--- broker/src/serve_health.rs
+++ broker/src/serve_health.rs
@@ -1,10 +1,11 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::{Duration, SystemTime}};
 
-use axum::{extract::State, http::StatusCode, routing::get, Json, Router};
-use serde::Serialize;
+use axum::{extract::{State, Path}, http::StatusCode, routing::get, Json, Router, TypedHeader, headers::{Authorization, authorization::Basic}};
+use serde::{Serialize, Deserialize};
+use shared::{crypto_jwt::Authorized, Msg, beam_id::ProxyId, config::CONFIG_CENTRAL};
 use tokio::sync::RwLock;
 
-use crate::health::{Health, VaultStatus, Verdict};
+use crate::health::{Health, VaultStatus, Verdict, ProxyStatus};
 
 #[derive(Serialize)]
 struct HealthOutput<'a> {
@@ -19,7 +20,8 @@ struct HealthOutputVault<'a> {
 
 pub(crate) fn router(health: Arc<RwLock<Health>>) -> Router {
     Router::new()
-        .route("/v1/health", get(handler))
+        .route("/v1/health", get(handler).post(register_proxy_health))
+        .route("/v1/health/:proxy_id", get(proxy_health))
         .with_state(health)
 }
 
@@ -48,4 +50,31 @@ async fn handler<'a>(
         },
     };
     (statuscode, Json(health_as_json))
+}
+
+
+async fn proxy_health(
+    State(state): State<Arc<RwLock<Health>>>,
+    Path(proxy): Path<ProxyId>,
+    auth: TypedHeader<Authorization<Basic>>
+) -> Result<Json<ProxyStatus>, StatusCode> {
+    if auth.password() != CONFIG_CENTRAL.monitoring_api_key {
+        return Err(StatusCode::UNAUTHORIZED)
+    }
+
+    if let Some(proxy_status) = state.read().await.proxies.get(&proxy) {
+        Ok(Json(proxy_status.clone()))
+    } else {
+        Err(StatusCode::SERVICE_UNAVAILABLE)
+    }
+}
+
+async fn register_proxy_health(
+    State(state): State<Arc<RwLock<Health>>>,
+    proxy_auth: Authorized,
+) -> StatusCode {
+    let proxy_id = proxy_auth.get_from().get_proxy_id(); 
+    state.write().await.proxies.insert(proxy_id, ProxyStatus::new());
+
+    StatusCode::OK
 }
