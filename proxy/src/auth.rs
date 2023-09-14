c94bf684667d457591a3cfb77d87a5f7c3aa548e
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-09-14 11:56:27 +0000

    Better beam proxy auth logging

--- proxy/src/auth.rs
+++ proxy/src/auth.rs
@@ -10,7 +10,7 @@ use shared::{
     config, config_proxy
 };
 
-use tracing::{debug, Span};
+use tracing::{debug, Span, debug_span, warn};
 
 pub(crate) struct AuthenticatedApp(pub(crate) AppId);
 
@@ -25,25 +25,30 @@ impl<S: Send + Sync> FromRequestParts<S> for AuthenticatedApp {
             [(header::WWW_AUTHENTICATE, SCHEME)],
         );
         if let Some(auth) = parts.headers.get(header::AUTHORIZATION) {
-            let auth = auth.to_str().map_err(|_| UNAUTH_ERR)?;
-            let mut auth = auth.split(' ');
-            if auth.next().unwrap_or("") != SCHEME {
+            let auth_str = auth.to_str().map_err(|_| UNAUTH_ERR)?;
+            let mut auth = auth_str.split(' ');
+            if auth.next() != Some(SCHEME) {
+                warn!(auth_str, "Invalid auth scheme");
                 return Err(UNAUTH_ERR);
             }
-            let client_id = auth.next().unwrap_or("");
-            let client_id = AppId::new(client_id).map_err(|_| UNAUTH_ERR)?;
-            let api_key_actual = config::CONFIG_PROXY
-                .api_keys
-                .get(&client_id)
-                .ok_or(UNAUTH_ERR)?;
+            let Some(client_id) = auth.next().and_then(|s| AppId::new(s).ok()) else {
+                warn!(auth_str, "Invalid app id");
+                return Err(UNAUTH_ERR);
+            };
+            let Some(api_key_actual) = config::CONFIG_PROXY.api_keys.get(&client_id) else {
+                warn!("App {client_id} not registered in proxy");
+                return Err(UNAUTH_ERR);
+            };
             let api_key_claimed = auth.next().ok_or(UNAUTH_ERR)?;
             if api_key_claimed != api_key_actual {
+                warn!("App {client_id} provided the wrong api key");
                 return Err(UNAUTH_ERR);
             }
             debug!("Request authenticated (ClientID {})", client_id);
             Span::current().record("from", AppOrProxyId::App(client_id.clone()).hide_broker());
             Ok(Self(client_id))
         } else {
+            warn!("No auth header provided");
             Err(UNAUTH_ERR)
         }
     }
