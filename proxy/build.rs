39c78e43f3d8bf7a82bd3e14970e59b436504784
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-10-10 11:57:10 +0000

    Remove frontend building from `build.rs`

--- proxy/build.rs
+++ proxy/build.rs
@@ -22,22 +22,4 @@ fn main() {
     build_data::set_BUILD_TIME();
     build_data::no_debug_rebuilds();
     println!("cargo:rustc-env=SAMPLY_USER_AGENT=Samply.Beam.{}/{}", env!("CARGO_PKG_NAME"), version());
-
-    // Compile the frontend with npm but not when we are using cross in the CI because we can compile it ahead of time and dont need to install npm in the docker containers that cross will spawn
-    if cfg!(feature = "monitor") && option_env!("CROSS_RUNNER").is_none() {
-        use std::process::Command;
-        std::env::set_current_dir("./monitor").expect("monitor directory has been deleted");
-        Command::new("npm")
-            .arg("install")
-            .spawn()
-            .expect("npm needs to be installed to compile monitoring feature")
-            .wait()
-            .expect("Failed to run npm install command");
-        Command::new("npm")
-            .args([ "run", "build" ])
-            .spawn()
-            .expect("npm needs to be installed to compile monitoring feature")
-            .wait()
-            .expect("Failed to run npm run build command");
-    }
 }
